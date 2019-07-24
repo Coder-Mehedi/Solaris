22fc4fa9695117cd5ed057e97ef5e83f6ca56267
Coder-Mehedi <mehedi5051@gmail.com>
2019-07-25 00:56:41 +0600

    changed circle from image pure css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,15 @@
 $(() => {
-    $('.result-bottom-btn button').on('click', function() {
-        $(this).next().slideToggle();
+    $("#section-2").on("click", function() {
+        $('html, body').animate({
+            scrollTop: ($('#about-us').offset().top)
+        }, 500);
     })
-})
+    $("#section-1").on("click", function() {
+        $('html, body').animate({
+            scrollTop: ($('#top').offset().top)
+        }, 500);
+
+
+    })
+
+});
